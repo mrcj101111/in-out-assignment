4c4c377b986c0ad9b2e03c994ddc8134f2347cd8
Corné <cornebotma@hotmail.com>
2019-04-22 10:15:39 +0200

    Amended writing to display correctly

--- input.py
+++ input.py
@@ -3,16 +3,16 @@ from datetime import datetime
 
 with open('input.csv', 'r') as csv_file:
     with open('new_output.csv', 'w') as csv_output:
-        writer = csv.writer(csv_output)
-        reader = csv.reader(csv_file)
+        writer = csv.writer(csv_output, delimiter=';', quoting=csv.QUOTE_NONE, escapechar=' ')
+        reader = csv.reader(csv_file, skipinitialspace=True)
 
         fields = next(reader)
         fields.append('Parsed')
-        writer.writerow([fields])
+        writer.writerow(fields)
 
         for item in reader:
             item.append(datetime.today().strftime('%Y-%m-%d'))
-            writer.writerow([item])
+            writer.writerow(item)
 
 
 
