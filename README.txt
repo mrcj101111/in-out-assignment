bbf152edcc4b3ae78482d8f855a0b139d4c9aa76
Corné <cornebotma@hotmail.com>
2019-04-22 10:53:56 +0200

    Fixed max line length

--- README.txt
+++ README.txt
@@ -1,14 +1,17 @@
 IN-Out-Assignment
 
 Getting Started:
-These instructions will get you a copy of the project up and running on your local machine for development and 
-testing purposes. See deployment for notes on how to deploy the project on a live system.
+These instructions will get you a copy of the project up and running on your
+local machine for development and 
+testing purposes. See deployment for notes on how to deploy the project on a
+live system.
 
 Prerequisites:
 -Python 3.7.3
 
 Installing:
--Visit the python website and download version 3.7.3 for your prefered operating system.
+-Visit the python website and download version 3.7.3 for your prefered
+operating system.
 https://www.python.org/downloads/
 
 -Run the installer and follow the steps.
--- csv_writer.py
+++ csv_writer.py
@@ -5,7 +5,8 @@ from datetime import datetime
 with open('input.csv', 'r') as csv_file:
     #Open csv file to write new data to
     with open('new_output.csv', 'w') as csv_output:
-        writer = csv.writer(csv_output, delimiter=';', quoting=csv.QUOTE_NONE, escapechar=' ')
+        writer = csv.writer(csv_output, delimiter=';', quoting=csv.QUOTE_NONE,
+        escapechar=' ')
         reader = csv.reader(csv_file, skipinitialspace=True)
 
         #Take the title row and append 'Parsed' to it
@@ -13,7 +14,8 @@ with open('input.csv', 'r') as csv_file:
         fields.append('Parsed')
         writer.writerow(fields)
 
-        #For each row after the title row, add another entry under the parsed column that displays the date and time
+        #For each row after the title row, add another entry under the parsed 
+        # column that displays the date and time
         for item in reader:
             item.append(datetime.today().strftime('%Y-%m-%d %X'))
             writer.writerow(item)
