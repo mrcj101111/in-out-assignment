64818fdaffe3ec508276bea5824f205232eabcfa
corne <corne@byteorbit.com>
2019-04-24 17:05:45 +0200

    Finished refactor

--- amend_csv.py
+++ amend_csv.py
@@ -14,7 +14,6 @@ class AmendCsv:
 
     # Create function to open the csv file to write to
     def write_file(self, input_file_name, output_file_name):
-        input_file_name = self.argument_parser().args.input
         input_file = self.read_file(input_file_name)
         writer = csv.writer(open(output_file_name, 'w', newline=''))
 
@@ -28,14 +27,13 @@ class AmendCsv:
             item.append(' ' + datetime.today().strftime('%Y-%m-%d %X'))
             writer.writerow(item)
 
-    def argument_parser(self):
-        parser = argparse.ArgumentParser()
-        parser.add_argument("-input", action='store', dest="input", help = "input filename", required=True)
-        parser.add_argument("-output", action='store', dest="output", help="output filename", required=True)
-        return parser.parse_args()
-
 
 if __name__ == "__main__":
 
+    # Parse file so that user can enter their input and output files in terminal
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-input", action='store', dest="input", help="input filename", required=True)
+    parser.add_argument("-output", action='store', dest="output", help="output filename", required=True)
+    args = parser.parse_args()
     csv_parser = AmendCsv()
-    csv_parser.write_file('Parsed', 'input.csv', 'new_output.csv')
+    csv_parser.write_file(args.input, args.output)
