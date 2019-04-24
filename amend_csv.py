4babe54f2e847d0e111bfb2369f7f3cb21e32051
corne <corne@byteorbit.com>
2019-04-24 16:42:34 +0200

    Changes to amend_csv.py

--- amend_csv.py
+++ amend_csv.py
@@ -1,4 +1,5 @@
 import csv
+import argparse
 from datetime import datetime
 
 
@@ -12,19 +13,26 @@ class AmendCsv:
         return csv.reader(open(file_name, 'r', newline=''))
 
     # Create function to open the csv file to write to
-    def write_file(self, title, input_file_name, output_file_name):
+    def write_file(self, input_file_name, output_file_name):
+        input_file_name = self.argument_parser().args.input
         input_file = self.read_file(input_file_name)
         writer = csv.writer(open(output_file_name, 'w', newline=''))
 
         # Take the title row and append an extra title
         fields = next(input_file)
-        fields.append(title)
+        fields.append('Parsed')
         writer.writerow(fields)
 
         # For each row after the title row, add another entry
         for item in input_file:
             item.append(' ' + datetime.today().strftime('%Y-%m-%d %X'))
             writer.writerow(item)
+
+    def argument_parser(self):
+        parser = argparse.ArgumentParser()
+        parser.add_argument("-input", action='store', dest="input", help = "input filename", required=True)
+        parser.add_argument("-output", action='store', dest="output", help="output filename", required=True)
+        return parser.parse_args()
 
 
 if __name__ == "__main__":
