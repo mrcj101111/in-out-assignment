39bff3fd42c8ae84dd52e612163b15f2576aae82
corne <corne@byteorbit.com>
2019-04-24 15:27:03 +0200

    Refactored amend_csv.py file

--- add_entry.py
+++
@@ -1,8 +0,0 @@
-from datetime import datetime
-
-class addEntry:
-    #For each row after the title row, add another entry 
-    def add_date(self, read_file, write_file):
-        for item in read_file:
-            item.append(' ' + datetime.today().strftime('%Y-%m-%d %X'))
-            write_file.writerow(item)
